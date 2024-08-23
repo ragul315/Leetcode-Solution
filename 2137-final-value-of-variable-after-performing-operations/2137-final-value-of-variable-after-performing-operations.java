2c057196e25d1775fc03b9aec81323640d21042c
Ragul J <raguljayaraj315@gmail.com>
2024-08-23 14:23:23 +0530

    Time: 1 ms (77.35%), Space: 42.7 MB (42.86%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x=0;
+        for(int i=0;i<operations.length;i++){
+            switch(operations[i]){
+                case "++X":
+                x++;
+                break;
+                case "X++":
+                x++;
+                break;
+                case "X--":
+                x--;
+                break;
+                case "--X":
+                x--;
+                default:
+                break;
+            }
+        }
+        return x;
+    }
+}
