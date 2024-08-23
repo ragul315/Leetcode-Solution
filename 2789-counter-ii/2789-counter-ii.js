b065d97a1a7e80aef35f22a8fc754cce4da31c7d
Ragul J <raguljayaraj315@gmail.com>
2024-08-23 14:10:12 +0530

    Time: 70 ms (7.11%), Space: 51.6 MB (58.69%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    var x = init;
+
+    return{
+        increment:()=>{
+            return ++x;
+        },
+        decrement:()=>{
+            return --x;
+        },
+        reset:()=>{
+            x=init;
+            return x;
+        }
+
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
